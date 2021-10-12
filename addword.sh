c427a5e63e64818a9179b3c4f288103a9a1ed365
Ѳад <45270032+nlogocntrcaphnt@users.noreply.github.com>
2021-10-12 16:18:52 +0300

    Add files via upload

--- addword.sh
+++ addword.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #translation="$(sdcv -u "My English - Greek" -n -e $1 | sed '1d' | sed '/-->/d')"
+export STARDICT_DATA_DIR=~/Documents/repositories/LaTeX_personal_dictionary/
 definition="$(sdcv -u "dictd_www.dict.org_gcide" $1 | sed '1d' | sed '/-->/d' | sed '/^$/d')"
 
-wordlist="$(grep 'emph' ~/Documents/repositories/dictionary/Words.tex | sed 's/.*{//' | sed 's/.$//' | xargs
-)"
+wordlist="$(grep 'emph' ~/Documents/repositories/LaTeX_personal_dictionary/Words.tex | sed 's/.*{//' | sed 's/.$//' | xargs)"
 
 if [[ "$wordlist" == *"$1"* ]]; then
 	printf "Word already exists. \n"
 else
-	head -n -2 ~/Documents/repositories/dictionary/Words.tex > /tmp/Words.tex
+	head -n -2 ~/Documents/repositories/LaTeX_personal_dictionary/Words.tex > /tmp/Words.tex
 	echo "\item[$\square$] \emph{ $1 }
 \begin{enumerate}
 \item{
@@ -20,13 +20,12 @@ $definition
 	echo "\end{itemize}
 \end{document}" >> /tmp/Words.tex
 
-	mv /tmp/Words.tex ~/Documents/repositories/dictionary/
-	pdflatex ~/Documents/repositories/dictionary/Words.tex Words.pdf
+	mv /tmp/Words.tex ~/Documents/repositories/LaTeX_personal_dictionary/
+	pdflatex -output-directory ~/Documents/repositories/LaTeX_personal_dictionary ~/Documents/repositories/LaTeX_personal_dictionary/Words.tex
 
 fi
 
-
-remaining="$(grep -o '\\emph' ~/Documents/repositories/dictionary/Words.tex | wc -l)"
+remaining="$(grep -o '\\emph' ~/Documents/repositories/LaTeX_personal_dictionary/Words.tex | wc -l)"
 printf "\n The number of words contained within the document are: $remaining"
 
 printf "\n \n$definition"
